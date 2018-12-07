d80a9e0da248de56ca6a016112aace739a4215ac
Aaron Chambers <achambers@gmail.com>
2018-12-07 15:18:23 +0000

    Test
    Test

---
+++ app/session/route.js
@@ -0,0 +1,14 @@
+import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
+
+export default Route.extend({
+  session: service(),
+
+  beforeModel() {
+    if (!this.session.user) {
+      this.transitionTo("login");
+    }
+  },
+
+  model() {}
+});
